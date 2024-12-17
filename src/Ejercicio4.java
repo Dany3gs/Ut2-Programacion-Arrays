6bdb5738c5571a771ed223013a5e71cd148d991b
Dany3gs <dsmmgc@gmail.com>
2024-12-17 00:36:27 +0000

    Ejercicio 4

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -3,9 +3,37 @@
 rellenarlo hasta que se llene el vector o se introduzca un número negativo. Entonces se debe
 imprimir el vector (sólo los elementos introducidos).*/
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = new int[10];
+        boolean continua = true;
+        int contador = 0;
+
+        System.out.println("Introduzca 10 números positivos o si desea parar indique un número negativo.");
+
+        for (int i = 0; i < array.length && continua; i++) {
+            array[i] = sc.nextInt();
+            if (continua && array[i] >= 0) {
+                contador++;
+                System.out.println("Indique otro número, o uno negativo si desea salir.");
+            }  else {
+                continua = false;
+            }
+        }
+
+        System.out.println("Las números indicadas son: ");
+        for (int i = 0; i < contador; i++) {
+            if (i == contador -1 ) {
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i] + ", ");
+            }
+        }
     }
 }
