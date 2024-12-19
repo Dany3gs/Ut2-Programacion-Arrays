bae52c67808dc2c3b8517419294ba6af8d9fbb1b
danymcboockair <dsmmgc@gmail.com>
2024-12-19 11:03:50 +0000

    Ejercicio 7 falta cálculo

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -3,9 +3,33 @@
 cada uno, pida valores para ‘vector1’ y ‘vector2’ y calcule vector3=vector1+vector2. Para ello hay
 que sumar cada una de las posiciones.*/
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] vector1 = new int[5];
+        int[] vector2 = new int[5];
+        int[] vector3 = new int[5];
+
+        System.out.println("Rellene vector 1:");
+        for (int i = 0; i < vector1.length; i++) {
+            vector1[i] = sc.nextInt();
+        }
+
+        System.out.println("Rellene vector 2:");
+        for (int i = 0; i < vector2.length; i++) {
+            vector2[i] = sc.nextInt();
+        }
+
+        System.out.println("Rellene vector 3:");
+        for (int i = 0; i < vector3.length; i++) {
+            // hacer cálculo aquí
+        }
+
 
     }
 }
